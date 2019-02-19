1e2c815af0ec5ad2f939dbfd7bebf117e84c65fa
Saneklic <saneklic@gmail.com>
2019-02-19 20:04:40 +0300

    Initial TextViewHello in MainActivity

--- app/src/main/java/com/my/homework/MainActivity.java
+++ app/src/main/java/com/my/homework/MainActivity.java
@@ -2,12 +2,17 @@ package com.my.homework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView TextViewHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextViewHello = findViewById(R.id.TextViewHello);
     }
 }
